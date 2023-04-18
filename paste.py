12861f9a72c308ee37df87bf11a48f9dcf5c17c1
curlew <git@curlew.is>
2023-04-19 02:14:45 +1000

    add setup function

--- paste.py
+++ paste.py
@@ -6,16 +6,32 @@ import sys
 import yaml
 
 URL = "https://pastebin.com/api/api_post.php"
+CONFIG_FILE = "config.yaml"
+
+def setup():
+    api_key = input("Enter Pastebin API key: ")
+    config = {"api_key": api_key}
+    try:
+        with open(CONFIG_FILE, "w") as f:
+            yaml.safe_dump(config, f)
+    except Exception as e:
+        print(e)
+        sys.exit(1)
 
 def main():
     parser = argparse.ArgumentParser()
+    parser.add_argument("--setup", action="store_true")
     parser.add_argument("-n", "--name")
     parser.add_argument("-e", "--expire", choices=["10M", "1H", "1D", "1W", "2W", "1M", "6M", "1Y"])
     parser.add_argument("-l", "--language")
     args = parser.parse_args()
 
+    if args.setup:
+        setup()
+        return
+
     try:
-        with open("config.yaml", "r") as f:
+        with open(CONFIG_FILE, "r") as f:
             config = yaml.safe_load(f)
             api_key = config["api_key"]
     except Exception as e:
